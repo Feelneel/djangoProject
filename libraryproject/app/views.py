5a6b0a765a363e83d296523f9837eb56d44e6040
Pratibha111222 <prabhuchaurasiya921>
2025-03-25 09:06:49 +0530

    djangoproject

--- libraryproject/app/views.py
+++ libraryproject/app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from . models import Book
 # Create your views here.
+
+# 
+# 
+# 
+# 
+
+
+
+from datetime import datetime
+
+
+def index(req):
+    allbooks = Book.objects.all()    #ORM
+    print(allbooks)
+    # context={"myname":"ITV"}
+    # return render(req,'index.html',context)
+
+    myname = "Neel"
+    # return render(req,'index.html',{"myname" : myname})
+    
+    print(datetime.now())
+    curdate = (datetime.now())
+    hour = datetime.now().hour
+    print(hour)
+    context = {"myname" : myname, "curdate": curdate, "hour": hour, "allbooks": allbooks}
+    
+    return render(req,'index.html', context)
